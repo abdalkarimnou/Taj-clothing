bb140185199a14a63333bfa73c46ae7d1006b374
Abd Alkarim <abdalkarimnou@gmail.com>
2026-03-27 14:40:50 +0300

    basic routing trining

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,31 +11,36 @@ class Directory extends React.Component {
                     title: 'hats',
                     imageUrl: 'https://media.gq.com/photos/57dc23c0beed5f275b337e16/16:9/w_1920,c_limit/fall-hats-gq-style-0816-01.jpgt',
                     size: 'large',
-                    id:1
+                    id:1,
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://media.gq-magazine.co.uk/photos/651ea0cb82e72cdf38c1655f/16:9/w_1920,c_limit/jackets.jpg',
                     size: 'large',
-                    id:2
+                    id:2,
+                    linkUrl: ''
                 },
                 {
                     title: 'sneakers',
                     imageUrl: 'https://www.footshop.eu/blog/wp-content/uploads/2023/04/KOV07592.jpg',
                     size: 'large',
-                    id:3
+                    id:3,
+                    linkUrl: ''
                 },
                   {
                     title: 'women',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/women.png',
                     size: 'large',
-                    id:4
+                    id:4,
+                    linkUrl: ''
                 },
                   {
                     title: 'men',
                     imageUrl: 'https://image.josbank.com/is/image/marketing/26-5329449-duo1-a-wc-2x.jpg?impolicy=FEWD&im=Resize,width=1024&qlt=85',
                     size: 'large',
-                    id:5
+                    id:5,
+                    linkUrl: ''
                 }
             ]
         };
@@ -43,11 +48,11 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({id, title, imageUrl, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         );
     }
-}   
+}  
 export default Directory;
--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,14 +1,17 @@
 import './menu-item.styles.scss';  
 import React from "react";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl , match}) => {
   return (
     <div
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-      className={`${size} menu-item`}
+
+      className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
+      <div className="background-image"     
+        style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}/>
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">Shop Now</span>
@@ -17,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
