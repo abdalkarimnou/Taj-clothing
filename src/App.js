733448b8bcdcd4b03275de3699566389e2ec183d
Abd Alkarim <abdalkarimnou@gmail.com>
2026-04-03 19:48:00 +0300

    sing-in and submit custom button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from './pages/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component'; 
 import Header from './components/header/header.compmnent';
-
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';  
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/sign-in' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
