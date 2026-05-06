4df541816dcf4e7d2db7f3e3baf299e46b8f1fe5
Abd Alkarim <abdalkarimnou@gmail.com>
2026-05-07 00:22:44 +0300

    first couple of erducers

--- src/App.js
+++ src/App.js
@@ -2,36 +2,43 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import ShopPage from './pages/shop/shop.component'; 
-import Header from './components/header/header.compmnent';
+import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';  
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+// Redux action that will update the `currentUser` in the store
+import { setCurrentUser } from './redux/user/user.actions';
 class App extends React.Component {
  
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-  unsubscribeFromAuth = null;
+ unsubscribeFromAuth = null;
   
   componentDidMount() {
+    // `setCurrentUser` is supplied via mapDispatchToProps and will dispatch
+    // the action to store the authenticated user in Redux.
+    const { setCurrentUser } = this.props;
+
+    // Subscribe to Firebase auth state changes. The listener returns an
+    // unsubscribe function which we keep as `this.unsubscribeFromAuth` so
+    // we can stop listening in componentWillUnmount and avoid leaks.
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
-      
+
+      // If a user is signed in, create/get their Firestore profile document
+      // and listen for snapshot updates. When the snapshot arrives we
+      // dispatch `setCurrentUser` with the user's id and stored data.
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
+      } else {
+        // If no user is signed in (userAuth is null), clear the currentUser
+        // in Redux so the app knows the user is signed out.
+        setCurrentUser(userAuth);
       }
-     else{ this.setState({ currentUser: userAuth });}
     });
   }
   componentWillUnmount() {
@@ -40,7 +47,7 @@ class App extends React.Component {
   render() {
   return (
     <div>
-      <Header currentUser={this.state.currentUser} />
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
@@ -51,5 +58,15 @@ class App extends React.Component {
 }
 
 }
+// mapDispatchToProps
+// - Provides action dispatcher props to the component. Here we expose
+//   `setCurrentUser` as a prop so the App can dispatch the action to update
+//   Redux state when auth changes occur.
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
 
-export default App;
+// Connect the App component to Redux with only dispatch props (no state).
+// This returns a higher-order component that injects `setCurrentUser` into
+// App's props and allows the component to dispatch user-related actions.
+export default connect(null, mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import './sign-in.styles.scss';
 import FormInput from "../form-input/form-inpu.component";
 import CustomButton from "../custom-button/custom-button.component";
-
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 class SignIn extends React.Component {
     constructor() {
         super();  
@@ -12,10 +11,16 @@ class SignIn extends React.Component {
             password: ''
         }
     }   
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' });
-    }   
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     handelChange = event => {
         const { value, name } = event.target;
